781507d95a03261988fa4b7890f90b2b6d9ac963
Jaya-Paliwal <jayapaliwal96@gmail.com>
2023-04-09 19:02:29 +0530

    My first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Tasklist";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Read Books",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Buy Hosting",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Learn Vue.JS",
+          completed: true,
+        },
+        {
+          id: 4,
+          title: "Read Newpaper",
+          completed: true,
+        }
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/Taskitem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <button @click.self="$emit('complete')" :class="className">
+       {{ task.title }}
+    </button> 
+    <button @click="$emit('remove')"><i class="far fa-trash-alt"></i></button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TaskItem",
+  props: ["task"],
+  computed: {
+    className() {
+      let classes = ['toggle'];
+      if(this.task.completed) {
+        classes.push('toggle-completed');
+      }
+      return classes.join(' ');
+    }
+  }
+};
+</script>
